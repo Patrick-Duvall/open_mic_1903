054ad0943880ad93102aa67b90cc43847625ece0
Patrick Duvall <duvallp208@gmail.com>
2019-03-20 09:32:02 -0600

    Complete iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -9,4 +9,12 @@ attr_reader :name, :jokes
     @jokes << joke
   end
 
+  def tell(user, joke)
+    user.learn(joke)
+  end
+#could act wierd if multiple jokes have same id, I might use sympols for jokes, also, comedians are more likely to mentally label their jokes as say :dog_joke
+  def joke_by_id(num)
+    @jokes.find{|joke| joke.id == num}
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -8,6 +8,7 @@ class UserTest<Minitest::Test
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
   end
 
   def test_has_name
@@ -21,6 +22,23 @@ class UserTest<Minitest::Test
     @sal.learn(@joke_2)
     assert_equal [@joke_1, @joke_2], @sal.jokes
 
+  end
+
+  def test_can_tell_jokes
+      @sal.learn(@joke_1)
+      @sal.tell(@ali, @joke_1)
+      assert_equal [@joke_1], @ali.jokes
+      @sal.learn(@joke_2)
+      @sal.tell(@ali, @joke_2)
+      assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_user_knows_jokes_by_id
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+    assert_equal @joke_1, @sal.joke_by_id(1)
+    @sal.tell(@ali, @joke_2)
+    assert_equal @joke_2, @ali.joke_by_id(2)
   end
 
 end
